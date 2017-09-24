f9df96f29ca6d60f541120ed8f8458bbff6c7784
Chris Davis <cdchris12@gmail.com>
2017-09-24 16:33:56 -0500

    API access now works as intended.

--- challenge.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-def main():
-	pass
-# End def
-
-if __name__ == "__main__":
-	main()
-# End if
---
+++ x_force.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+import urllib
+import urllib2
+import json
+import os.path
+import time
+import re
+from datetime import datetime, timedelta
+import sys
+import base64
+
+
+BASEurl = "https://api.xforce.ibmcloud.com/"
+
+sys.path.append('./')
+import iprep_conf as IC
+
+authstring = base64.encodestring(IC.xfex_cred).replace('\n','')
+
+yesterday = datetime.now() - timedelta(days=1)
+YEST = yesterday.strftime('20%y-%m-%dT00:00:00Z')
+
+headers = {
+    "Authorization": "Basic %s " % authstring,
+    "Accept": "application/json",
+    'User-Agent': 'Mozilla 5.0'}
+
+def getip(ip):
+    try:
+        furl = BASEurl + "ipr/%s" % ip
+        furl2 = BASEurl + "ipr/malware/%s" % ip
+
+        request = urllib2.Request(furl, None, headers)
+        data = urllib2.urlopen(request)
+        data2 = json.loads(data.read())
+
+        request = urllib2.Request(furl2, None, headers)
+        data = urllib2.urlopen(request)
+        data3 = json.loads(data.read())
+
+        merged_dict = {key: value for (key, value) in (data2.items() + data3.items())}
+        return merged_dict
+        #return str(data2)
+        #return [data2[u"history"][0]["geo"]["country"], data2[u"score"], data2[u"reason"], data2[u"categoryDescriptions"]]
+    except:
+        return [str(data2), "Ups", "Ups", "ups"]
+    # End try/except block
+# End def
+
+def extractIP(text):
+    ip = re.compile(r"\b(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\b")
+    return ip.findall(text)[0]
+# End def
+
+if __name__ == "__main__":
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-i", "--ip", dest="ip" , default=None, help="get IP intel", metavar="IP_Address")
+
+    (options, args) = parser.parse_args()
+
+    if options.ip is not None:
+        res = extractIP(options.ip)
+        if not res:
+            print "You entered an invalid IP address. Please retry your query with a valid address."
+            sys.exit(1)
+        else:
+            ip_res = getip(res)
+        # End if/else block
+    else:
+        "Please specify an IP address to retrieve information for."
+        sys.exit(1)
+    # End if/else block
+# End if
+
